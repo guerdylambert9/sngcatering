c26754a742158f257e5875fd07b1770da8609f12
Guerdy <guerdylambert9@gmail.com>
2025-04-28 15:52:36 -0400

    Implemented the menu page and the order functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/HeaderComponent/Header'
-import Hero from './components/HeroComponent/Hero'
-import Contact from './components/ContactComponent/Contact'
+//import Header from './components/HeaderComponent/Header'
+//import Hero from './components/HeroComponent/Hero'
+//import Contact from './components/ContactComponent/Contact'
+
+import Home from './pages/Home'
+import Menu from './pages/Menu'
+import Cart from './pages/Cart'
 
 import FeaturedDishes from './components/FeaturedDishes/FeaturedDishes'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <Header/>
-      <Hero/>
-      <FeaturedDishes/>
-      <Contact/>  
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/menu" element={<Menu />}/>
+      <Route path="/cart" element={<Cart />}/>
+    </Routes>
   )
 
 }
